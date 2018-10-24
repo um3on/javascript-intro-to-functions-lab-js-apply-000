df788b2348811e17a79a921b558d0cfd812fc7bc
helpful-founder-6136 <mariam.tamimi@gmail.com>
2018-10-24 13:14:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ fucntion logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if(string === whisper(string)) {
-    console.log("I can't hear you!");
+    return "I can't hear you!";
   }
   else if (string === shout(string)) {
     return "YES INDEED!";
